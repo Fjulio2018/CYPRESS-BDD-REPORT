628c763ff9c174ff5d1de3071f0aeb8f4b0cbc68
Francisnasc <juliomariano2005@yahoo.com.br>
2024-09-02 14:12:42 -0300

    CI Actions-cypress.io

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@
 const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
 module.exports = defineConfig({
+  projectId: 'dny4ex',
   e2e: {
     baseUrl: 'https://automationpratice.com.br/',
     specPattern: "**/*.feature",
